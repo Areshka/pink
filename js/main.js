389a5d2351eb9d5024ee7ba20cf50ffc193b102c
Oleg <oleg1990moskalenko@gmail.com>
2017-09-15 14:55:04 +0300

    added adaptive menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu').toggleClass('menu_state_open');
+    });
+  });
+})(jQuery);
+
 // main-nav open-close
-var navMain = document.querySelector(".main-nav");
-var navToggle = navMain.querySelector(".main-nav__toggle");
-
-navMain.classList.remove("main-nav--nojs");
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
-});
+// var navMain = document.querySelector(".main-nav");
+// var navToggle = navMain.querySelector(".main-nav__toggle");
+//
+// navMain.classList.remove("main-nav--nojs");
+//
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains("main-nav--closed")) {
+//     navMain.classList.remove("main-nav--closed");
+//     navMain.classList.add("main-nav--opened");
+//   } else {
+//     navMain.classList.add("main-nav--closed");
+//     navMain.classList.remove("main-nav--opened");
+//   }
+// });
 // end main-nav open-close
 
 // reviews slider
